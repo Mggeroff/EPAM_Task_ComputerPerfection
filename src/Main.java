7508ad234bf0b3c5c5e5451044ae31f08c69398f
Roman Mager <roman.mager@outlook.com>
2018-07-10 23:59:50 +0300

    Add logger.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-public class Main {
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Main {
+    static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        logger.log(Level.INFO, "Hello world!");
     }
 }

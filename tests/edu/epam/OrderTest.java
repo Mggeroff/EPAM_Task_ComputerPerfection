c7c577b29c4b0a8a2556e91a3fd8957ca400d37d
Roman Mager <roman.mager@outlook.com>
2018-08-09 09:13:25 +0300

    Add InvoiceReader and boilerplate methods for OrderTest.

---
+++ tests/edu/epam/OrderTest.java
@@ -0,0 +1,38 @@
+package edu.epam;
+
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+public class OrderTest {
+    @DataProvider(name = "Name")
+    public static Object[][] Name() {
+        return null;
+    }
+
+
+    @Test
+    public void testAddComputerCase() {
+    }
+
+    @Test
+    public void testGetOrderNumber() {
+    }
+
+    @Test
+    public void testChangeNumberOfOrderedComputers() {
+    }
+
+    @Test
+    public void testChangeComputerName() {
+    }
+
+    @Test
+    public void testShowInfo() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
